0352df4987cc9c28abdb17614f21a2268008d945
chisty sohel <chistysohail@gmail.com>
2024-05-02 14:47:50 -0400

    Add project files.

---
+++ PlatformSpecificConsoleApp/Program.cs
@@ -0,0 +1,10 @@
+﻿using PlatformSpecificLibrary;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var lib = new Class1();
+        lib.Run();
+    }
+}
---
+++ PlatformSpecificLibrary/Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace PlatformSpecificLibrary
+{
+    public class Class1
+    {
+        public void Run()
+        {
+#if LINUX
+#error "This library cannot be compiled on Linux."
+#else
+            Console.WriteLine("This library is running on a supported OS.");
+#endif
+        }
+    }
+}
